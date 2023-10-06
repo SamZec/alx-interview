9f73de539327c6fa18506356bd992367d4549d09
SamZec <samzec4@gmail.com>
2023-10-06 08:50:08 +0000

    Correct output error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,20 +7,25 @@ def canUnlockAll(boxes):
     index = 0
     keys = list(set(boxes[0]) | {0})
     opened = True
+    _boxes = []
 
     while opened:
         opened = False
         for key in join(boxes, keys[index:]):
+            if key <= len(boxes):
+                _boxes.append(key)
             if key not in keys:
                 keys.append(key)
                 index += 1
                 opened = True
-    return len(keys) == len(boxes)
+    return len(set(_boxes)) == len(boxes)
 
 
 def join(boxes, keys):
     """open a box with the key"""
     res = []
     for key in keys:
+        if key > len(boxes) - 1:
+            continue
         res += boxes[key]
     return res
